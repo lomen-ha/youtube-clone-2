e669d009e9770fafee9710b9db798e504d15729e
hakim <the9616@naver.com>
2022-04-06 16:07:25 +0900

    commit

--- src/components/video-list/video-list.jsx
+++ src/components/video-list/video-list.jsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const VideoList = ({ props }) => (
   <ul className={styles.video_list}>
     {props.map((list) => (
-      <VideoItem videoItem={list} key={props.id} />
+      <VideoItem videoItem={list} key={list.id} />
     ))}
   </ul>
 );
